6181bc29c48728d2eac7ee3a6a39c8fe5d968d65
Medhad <medhad@gmail.com>
2023-04-16 13:58:36 -0400

    Initiated xcode project

---
+++ YelpiOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YelpiOSApp
+//
+//  Created by Medhad Ashraf Islam on 16/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YelpiOSAppUITests/YelpiOSAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  YelpiOSAppUITestsLaunchTests.swift
+//  YelpiOSAppUITests
+//
+//  Created by Medhad Ashraf Islam on 16/4/23.
+//
+
+import XCTest
+
+final class YelpiOSAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
