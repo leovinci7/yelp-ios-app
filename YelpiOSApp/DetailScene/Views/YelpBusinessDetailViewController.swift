3b1c9666dbb6c87db0816d8dc22bef2ac9ed0ae2
Medhad <medhad@gmail.com>
2023-04-21 05:26:40 +0600

    Added outlet connection between BusinessView and controller

--- YelpiOSApp/DetailScene/Views/YelpBusinessDetailViewController.swift
+++ YelpiOSApp/DetailScene/Views/YelpBusinessDetailViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class YelpBusinessDetailViewController:UIViewController {
     
+    @IBOutlet weak var mainBusinessImage: UIImageView!
+    @IBOutlet weak var imageOne: UIImageView!
+    @IBOutlet weak var imageTwo: UIImageView!
+    @IBOutlet weak var imageThree: UIImageView!
+    
+    @IBOutlet weak var businessName: UILabel!
+    @IBOutlet weak var businessRating: UILabel!
+    
+    @IBOutlet weak var businessPriceCategory: UILabel!
+    @IBOutlet weak var businessServiceCategory: UILabel!
+    @IBOutlet weak var businessOpenStatus: UILabel!
+    @IBOutlet weak var businessOperatingHour: UIStackView!
+    
     var selectedItem:String?
     
     override func viewDidLoad() {
