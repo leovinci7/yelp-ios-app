0503c20afdbf527ac8c13c269c7478008155ef3b
Medhad <medhad@gmail.com>
2023-04-21 21:17:17 +0600

    Reorganized the code

--- YelpiOSApp/Helper/ViewHelper.swift
+++ YelpiOSApp/Helper/ViewHelper.swift
@@ -9,21 +9,6 @@ import Foundation
 
 struct ViewHelper {
     
-    public static func starRatingStringOld(rating: Double) -> String {
-        let fullStar = "★"
-        let halfStar = "☆"
-        
-        let numberOfFullStars = Int(rating)
-        let hasHalfStar = rating.truncatingRemainder(dividingBy: 1) >= 0.5
-        
-        var ratingString = String(repeating: fullStar, count: numberOfFullStars)
-        if hasHalfStar {
-            ratingString.append(halfStar)
-        }
-        
-        return ratingString
-    }
-    
     public static func starRatingString(rating: Double) -> String {
         let fullStar = "★"
         let halfStar = "½"
